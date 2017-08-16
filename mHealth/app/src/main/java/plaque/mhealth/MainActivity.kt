c01d033e12614f601df08dffbdf4fab8ead4a824
plaque <haikubee95@gmail.com>
2017-08-16 22:52:28 +0200

    added activities to buttons

--- mHealth/app/src/main/java/plaque/mhealth/MainActivity.kt
+++ mHealth/app/src/main/java/plaque/mhealth/MainActivity.kt
@@ -1,12 +1,22 @@
 package plaque.mhealth
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        meds_button.setOnClickListener { _ -> startActivity(Intent(this, MedsActivity::class.java)) }
+
+        results_button.setOnClickListener { _ -> startActivity(Intent(this, ResultsActivity::class.java)) }
+
+        sitters_button.setOnClickListener { _ -> startActivity(Intent(this, SittersActivity::class.java)) }
+
+        pupils_button.setOnClickListener { _ -> startActivity(Intent(this, PupilsActivity::class.java)) }
     }
 }
