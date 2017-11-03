59300b31396adabf068138d1d2a6d89f17774752
plaque <haikubee95@gmail.com>
2017-11-03 21:07:18 +0100

    Next design changes - main activity

--- mHealth/app/src/main/java/plaque/mhealth/ui/main_screen/MainActivity.kt
+++ mHealth/app/src/main/java/plaque/mhealth/ui/main_screen/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main1.*
 import plaque.mhealth.R
 import plaque.mhealth.database.DataStore
 import plaque.mhealth.mHealthApp
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main1)
 
         mHealthApp.mainComponent.inject(this)
 
